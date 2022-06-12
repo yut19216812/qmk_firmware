2e729081af553aedca24762c2e7dd70550407292
adsh1y0 <adsh1y0@gmail.com>
2022-06-12 13:31:00 +0900

    wip

--- keyboards/keebio/nyquist/keymaps/adsh1y0/keymap.c
+++ keyboards/keebio/nyquist/keymaps/adsh1y0/keymap.c
@@ -20,6 +20,9 @@ enum custom_keycodes {
 #define ALT_BSPC ALT_T(KC_BSPC)
 #define SPC_SFT SFT_T(KC_SPC)
 
+#define LT_SPC LT(_LOWER, KC_SPC)
+#define LT_ENT LT(_RAISE, KC_ENT)
+
 // Underglow setup
 #define RGBLIGHT_SLEEP
 #define RGBLIGHT_ANIMATIONS
@@ -40,11 +43,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `----------------------------------------'     '-----------------------------------------'
  */
 [_QWERTY] = LAYOUT(
-  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,  KC_5,       KC_6,    KC_7,    KC_8,     KC_9,    KC_0,       KC_MINS,
-  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,       KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,       KC_ENT,
-  KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,  KC_G,       KC_H,    KC_J,    KC_K,     KC_L,    KC_MV_SCLN, KC_QUOT,
-  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,       KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,    KC_ESC_EN,
-  KC_LANG2,  KC_LCTL, KC_LALT, KC_LGUI, LOWER, SPC_SFT,    KC_ENT,  RAISE,  ALT_BSPC, KC_RALT,  KC_RCTL,    KC_LANG1
+  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,   KC_5,          KC_6,    KC_7,    KC_8,     KC_9,    KC_0,       KC_MINS,
+  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,          KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,       KC_ENT,
+  KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,   KC_G,          KC_H,    KC_J,    KC_K,     KC_L,    KC_MV_SCLN, KC_QUOT,
+  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,          KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,    KC_ESC_EN,
+  KC_LANG2,  KC_LCTL, KC_LALT, KC_LGUI, LT_SPC, LT_SPC,        LT_ENT, LT_ENT,  ALT_BSPC, KC_RALT,  KC_RCTL, KC_LANG1
   ),
 
 /* Lower
@@ -113,9 +116,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 //A description for expressing the layer position in LED mode.
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//   return update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
-// }
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
+}
 
 // void persistent_default_layer_set(uint16_t default_layer) {
 //   eeconfig_update_default_layer(default_layer);
@@ -132,34 +135,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
+    // case LOWER:
+    //   if (record->event.pressed) {
+    //     layer_on(_LOWER);
+    //     update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    //   } else {
+    //     layer_off(_LOWER);
+    //     update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    //   }
+    //   return false;
+    //   break;
+    // case RAISE:
+    //   if (record->event.pressed) {
+    //     layer_on(_RAISE);
+    //     update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    //   } else {
+    //     layer_off(_RAISE);
+    //     update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    //   }
+    //   return false;
+    //   break;
+    // case ADJUST:
+    //   if (record->event.pressed) {
+    //     layer_on(_ADJUST);
+    //   } else {
+    //     layer_off(_ADJUST);
+    //   }
+    //   return false;
+    //   break;
     case KC_ESC_EN:
       if (record->event.pressed) {
         register_code(KC_ESC);
@@ -168,6 +171,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         unregister_code(KC_ESC);
         unregister_code(KC_LANG2);
       }
+      return false;
       break;
   }
   return true;
