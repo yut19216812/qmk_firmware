0d906ede209ce634c463256c97d8c7e15fd76995
adsh1y0 <adsh1y0@gmail.com>
2022-07-18 23:10:46 +0900

    wip

--- keyboards/keebio/nyquist/keymaps/adsh1y0/config.h
+++ keyboards/keebio/nyquist/keymaps/adsh1y0/config.h
@@ -22,9 +22,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 // #define MASTER_RIGHT
 // #define EE_HANDS
-#define PERMISSIVE_HOLD
 #define RETRO_TAPPING
-#define TAPPING_TERM 100
+#define PERMISSIVE_HOLD
+// #define TAPPING_TERM 100
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
