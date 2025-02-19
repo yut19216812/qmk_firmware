41c7f656e95ce722bd323b40f787650fce50cad6
yut <yut19216811@gmail.com>
2025-02-20 06:49:48 +0900

    wip

--- keyboards/bastardkb/tbkmini/keymaps/adsh1y0/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/adsh1y0/keymap.c
@@ -17,16 +17,16 @@
 
 #include QMK_KEYBOARD_H
 
-#define KC_L1_SCLN LT(_L1, KC_SCLN)
-#define ALT_BS ALT_T(KC_BSPC)
+#define LT3_SCLN LT(_L3, KC_SCLN)
 #define SFT_SPC SFT_T(KC_SPC)
 #define SFT_ENT SFT_T(KC_ENT)
 #define SFT_TAB SFT_T(KC_TAB)
 #define CTL_TAB CTL_T(KC_TAB)
 #define CTL_Z CTL_T(KC_Z)
 #define CTL_SLSH CLL_T(KC_SLSH)
-#define LT_SPC LT(_L1, KC_SPC)
-#define LT_ENT LT(_T2, KC_ENT)
+// #define LT_SPC LT(_L1, KC_SPC)
+// #define LT_ENT LT(_T2, KC_ENT)
+#define LT1_BSPC LT(_L1, KC_BSPC)
 
 enum layer_names {
     _QW,
@@ -38,57 +38,52 @@ enum layer_names {
 
 enum original_keycodes {
   KC_ESC_EN = SAFE_RANGE,
-  YBI_WIN_FRONT,
-  YBI_WIN_LEFT,
-  YBI_WIN_RIGHT,
-  YBI_WIN_UP,
-  YBI_WIN_DOWN,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QW] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-           KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                                     KC_H, KC_J, KC_K, KC_L, KC_L1_SCLN, KC_RCTL,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                                       KC_H, KC_J, KC_K, KC_L, LT3_SCLN, KC_RCTL,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                                     KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC_EN,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                                       KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC_EN,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-           KC_LGUI, MO(_L1), SFT_SPC,      SFT_ENT,  MO(_L2), ALT_BS
+        KC_LGUI, MO(_L1), SFT_SPC,      SFT_ENT,  LT1_BSPC, KC_RALT
         //`--------------------------'  `--------------------------'
         ),
 
     [_L1] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                            YBI_WIN_LEFT, YBI_WIN_DOWN, YBI_WIN_UP, YBI_WIN_RIGHT,  _______, _______,
+        KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+        KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                           KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_TRNS, _______, _______, _______, _______, _______,                         _______, _______, _______, _______,  _______, _______,
-        //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-        KC_LGUI, MO(_L3), SFT_SPC,      SFT_ENT,  MO(_L3), ALT_BS
-        //`--------------------------'  `-------------------------'
+        KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
+        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_LGUI, MO(_L3), SFT_SPC,      SFT_ENT,  MO(_L3), KC_RALT
+        //`--------------------------'  `--------------------------'
         ),
-
 
     [_L2] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_TRNS,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
+        KC_TRNS, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, _______,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_TRNS,  _______, KC_LCBR, KC_DQT,  KC_RCBR, _______,                         KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV,
+        KC_TRNS, _______, _______, _______, _______, _______,                          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_TRNS,  _______, KC_LBRC, KC_QUOT, KC_RBRC, _______,                         KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
-        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LGUI, MO(_L3), SFT_SPC,      SFT_ENT,  MO(_L3), ALT_BS
-        //`--------------------------'  `--------------------------'
+        KC_TRNS, _______, _______, _______, _______, _______,                          _______, _______, _______, _______,  _______, _______,
+        //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+        KC_LGUI, MO(_L3), SFT_SPC,      SFT_ENT,  MO(_L3), KC_RALT
+        //`--------------------------'  `-------------------------'
         ),
+
 
     [_L3] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_TRNS, _______, _______, _______, KC_LNG2, YBI_WIN_FRONT,                    _______, KC_LNG1, _______, _______, _______, _______,
+        KC_TRNS, _______, _______, _______, KC_LNG2, _______,                          _______, KC_LNG1, _______, _______, _______, _______,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_TRNS, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, _______,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -117,58 +112,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         unregister_code(KC_ESC);
         unregister_code(KC_LNG2);
-      }
-      return false;
-      break;
-    case YBI_WIN_FRONT:
-      if (record->event.pressed) {
-        register_code(KC_LSFT);
-        register_code(KC_LALT);
-        register_code(KC_SPACE);
-      } else {
-        unregister_code(KC_LSFT);
-        unregister_code(KC_LALT);
-        unregister_code(KC_SPACE);
-      }
-      return false;
-      break;
-    case YBI_WIN_LEFT:
-      if (record->event.pressed) {
-        register_code(KC_LALT);
-        register_code(KC_H);
-      } else {
-        unregister_code(KC_LALT);
-        unregister_code(KC_H);
-      }
-      return false;
-      break;
-    case YBI_WIN_RIGHT:
-      if (record->event.pressed) {
-        register_code(KC_LALT);
-        register_code(KC_L);
-      } else {
-        unregister_code(KC_LALT);
-        unregister_code(KC_L);
-      }
-      return false;
-      break;
-    case YBI_WIN_UP:
-      if (record->event.pressed) {
-        register_code(KC_LALT);
-        register_code(KC_K);
-      } else {
-        unregister_code(KC_LALT);
-        unregister_code(KC_K);
-      }
-      return false;
-      break;
-    case YBI_WIN_DOWN:
-      if (record->event.pressed) {
-        register_code(KC_LALT);
-        register_code(KC_J);
-      } else {
-        unregister_code(KC_LALT);
-        unregister_code(KC_J);
       }
       return false;
       break;
